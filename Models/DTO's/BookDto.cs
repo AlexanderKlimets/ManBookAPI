0bc764c37e6a117bcd02409d789d360c6548a0e3
Alexander <sashaklimets@list.ru>
2021-08-29 22:17:54 +0300

    Complete Task 3
    Major changes in project structure and introduction dependency injection into the project

---
+++ Models/DTO's/BookDto.cs
@@ -0,0 +1,35 @@
+﻿using DataBaseRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DataBaseRepository.DTO_s
+{
+    /// <summary>
+    /// Класс, представляющий книгу.
+    /// </summary>
+    public class BookDto
+    {
+        /// <summary>
+        /// Название книги.
+        /// </summary>
+        [Required]
+        [JsonInclude]
+        public string Title { get; set; }
+        /// <summary>
+        /// Имя автора книги.
+        /// </summary>
+        [Required]
+        [JsonInclude]
+        public AuthorDto Author { get; set; }
+        /// <summary>
+        /// Жанр книги.
+        /// </summary>
+        [Required]
+        public List<GenreDto> Genres { get; set; }
+    }
+}
